9d19d21d120fcc5c77907b091240dea1dbe54b13
minseongkim <kyrok67@naver.com>
2021-04-14 05:46:06 +0900

    Initial Commit

---
+++ Passing Data Between VC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passing Data Between VC
+//
+//  Created by MIN SEONG KIM on 2021/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
