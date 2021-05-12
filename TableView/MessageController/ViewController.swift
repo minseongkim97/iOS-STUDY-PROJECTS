032d3d523dd033fea48f87acd0d554d229fcbd1d
minseongkim <kyrok67@naver.com>
2021-05-13 04:28:57 +0900

    Add swipe button & UISearchController.

--- TableView/MessageController/ViewController.swift
+++ TableView/MessageController/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     var filteredName: [String] = []
     var filteredMessage: [String] = []
-
+    let searchController = UISearchController(searchResultsController: nil)
     
    
     
@@ -26,21 +26,25 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet private weak var searchBar: UISearchBar! {
-        didSet {
-            self.searchBar.delegate = self
-            self.searchBar.backgroundImage = UIImage()
-        }
-    }
+//    @IBOutlet private weak var searchBar: UISearchBar! {
+//        didSet {
+//            self.searchBar.delegate = self
+//            self.searchBar.backgroundImage = UIImage()
+//        }
+//    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchController.searchResultsUpdater = self
         
-        // navigationbar 선없애기
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = false
+        searchController.searchBar.placeholder = "검색"
+        self.navigationItem.searchController = searchController
+        
+//        // navigationbar 선없애기
+//        self.navigationController?.navigationBar.shadowImage = UIImage()
+//        self.navigationController?.navigationBar.isTranslucent = false
 
       
     
@@ -66,18 +70,21 @@ extension ViewController: UITableViewDataSource {
         return filteredName.count
     }
     
-    // swipe to delete
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            filteredName.remove(at: indexPath.row)
-            filteredMessage.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-              
-        }
-        else if editingStyle == .insert {
-            
-        }
-    }
+//    // swipe to delete
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            filteredName.remove(at: indexPath.row)
+//            filteredMessage.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//
+//        }
+//        else if editingStyle == .insert {
+//
+//        }
+//    }
+    
+    
+    
 }
 
 extension ViewController: UITableViewDelegate {
@@ -85,36 +92,101 @@ extension ViewController: UITableViewDelegate {
         
         // cell 클릭 후 회색 잔상 안남기기
         tableView.deselectRow(at: indexPath, animated: true)
-    
-        
     }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let pin = UIContextualAction(style: .normal, title: "Pin") { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
+                    print("Pin Clicked")
+                    success(true)
+        }
+        pin.backgroundColor = .orange
+        pin.image = UIImage(systemName: "pin.fill")
+        
+        return UISwipeActionsConfiguration(actions:[pin])
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let alarm = UIContextualAction(style: .normal, title: "Alarm") { (UIContextualAction, UIView, succes: @escaping (Bool) -> Void) in
+            print("Alarm Clicked")
+            succes(true)
+        }
+        alarm.backgroundColor = .purple
+        alarm.image = UIImage(systemName: "bell.slash.fill")
+        
+        let delete = UIContextualAction(style: .normal, title: "delete") { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
+            self.filteredName.remove(at: indexPath.row)
+            self.filteredMessage.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            success(true)
+        }
+        delete.backgroundColor = .red
+        delete.image = UIImage(systemName: "trash.fill")
+        
+        return UISwipeActionsConfiguration(actions: [delete,alarm])
+    }
+    
+    // 셀 높이 지정해서 title이 같이 나오게 할 수 있음
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 80
+//    }
 }
 
-// 검색 기능
-extension ViewController: UISearchBarDelegate {
-    // 검색 창 안의 텍스트가 여기 안의 코드를 실행
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-      
+//// 검색 기능
+//extension ViewController: UISearchBarDelegate {
+//    // 검색 창 안의 텍스트가 여기 안의 코드를 실행
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//
+//        filteredName = []
+//        filteredMessage = []
+//
+//        if searchText == "" {
+//            filteredName = Message.name
+//            filteredMessage = Message.message
+//
+//        } else {
+//            for names in 0..<Message.name.count {
+//                if Message.name[names].lowercased().contains(searchText.lowercased()) {
+//                    filteredName.append(Message.name[names])
+//                    filteredMessage.append((Message.message[names]))
+//                }
+//            }
+//
+//
+//        }
+//        DispatchQueue.main.async {
+//            self.tableView.reloadData()
+//        }
+//
+//    }
+//}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
         filteredName = []
         filteredMessage = []
-      
-        if searchText == "" {
+
+        // searchController의 searchBar
+        guard let text = searchController.searchBar.text?.lowercased() else { return }
+        
+        if text == "" {
             filteredName = Message.name
             filteredMessage = Message.message
-           
+
         } else {
             for names in 0..<Message.name.count {
-                if Message.name[names].lowercased().contains(searchText.lowercased()) {
+                if Message.name[names].lowercased().contains(text.lowercased()) {
                     filteredName.append(Message.name[names])
                     filteredMessage.append((Message.message[names]))
                 }
             }
-            
-        
+
+
         }
         DispatchQueue.main.async {
             self.tableView.reloadData()
         }
-       
+
     }
+    
+    
 }
