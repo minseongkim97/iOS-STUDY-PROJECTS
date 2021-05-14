14548b87c98c74bed476e6b217597b766515c509
minseongkim <kyrok67@naver.com>
2021-05-15 02:26:24 +0900

    Start-CollectionView

---
+++ Start CollectionView/Controller/MyCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  MyCollectionViewCell.swift
+//  Start CollectionView
+//
+//  Created by MIN SEONG KIM on 2021/05/15.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    static let identifier = "MyCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    // 셀의 재사용 문제를 방지
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        label.text = nil
+        imageView.image = nil
+//        backgroundColor = .systemBackground
+    }
+    
+    public func configure(with image: UIImage, text: String) {
+        imageView.image = image
+        label.text = text
+        backgroundColor = .black
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MyCollectionViewCell", bundle: nil)
+    }
+    
+  
+
+}
---
+++ Start CollectionView/Controller/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Start CollectionView
+//
+//  Created by MIN SEONG KIM on 2021/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        didSet {
+            self.collectionView.dataSource = self
+            self.collectionView.delegate = self
+            self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+            self.collectionView.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+            
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
+        
+        // 셀 재사용 문제 예시
+//        if indexPath.row == 5 {
+//            cell.backgroundColor = .black
+//        }
+        
+        
+        // 셀 UI 설정
+        cell.configure(with: UIImage(systemName: "house")!, text: "custom \(indexPath.row)")
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("you tapped row")
+    }
+    
+}
+
+// custom layout collection view
+// collection view에서 여백과 패딩, 아이템 사이즈를 지정
+// cell 들이 기기의 가장자리쪽으로 붙게 된다 why?
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.bounds.width/3-3, height: collectionView.bounds.width/3-3)
+    }
+    
+    // 지정된 섹션의 여백을 반환하는 메서드.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 30, left: 1, bottom: 15, right: 1)
+    }
+    
+    // 지정된 섹션의 행 사이 간격 최소 간격을 반환하는 메서드. scrollDirection이 horizontal이면 수직이 행이 되고 vertical이면 수평이 행이 된다.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        30
+    }
+    
+    // 지정된 섹션의 셀 사이의 최소간격을 반환하는 메서드.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        1
+    }
+    
+}
